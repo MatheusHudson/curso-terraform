3ce404cdd24cf9f9f3314582d909ccab02819206
MatheusHudson <matheushudson2013@gmail.com>
2021-10-11 11:18:35 -0300

    v1.2

--- main.tf
+++ main.tf
@@ -2,14 +2,26 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.14.0"
+      version = "2.15.0"
     }
   }
 }
 
 provider "docker" {
+   host = "tcp://localhost:2375"
 }
 
 resource "docker_image" "docusaurus-zup" {
   name = "public.ecr.aws/zup-academy/docusaurus-zup:latest"
+}
+
+# Start a container
+resource "docker_container" "docusaurus-zup" {
+  name  = "docusaurus-zup"
+  image = docker_image.docusaurus-zup.latest
+
+  # map
+  ports {
+    internal = 3000
+  }
 }
